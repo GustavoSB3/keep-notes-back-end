8405b00999c6a4efc74ce86f8edd8c32663fba87
GustavoSB3 <161391954+GustavoSB3@users.noreply.github.com>
2026-06-10 22:56:00 -0300

    feat: add new routes for notes

--- app/database.py
+++ app/database.py
@@ -1,29 +1,17 @@
-from dotenv import load_dotenv
 import os
 import mysql.connector
+from dotenv import load_dotenv
 
-load_dotenv()
-
-try:
-    conexao = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME")
-    )
-
-    cursor = conexao.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM usuarios;")
-
-    for linha in cursor.fetchall():
-        print(linha)
-
-except mysql.connector.Error as err:
-    print(f"Erro ao conectar ou consultar o banco: {err}")
-
-finally:
-    if conexao.is_connected():
-        cursor.close()
-        conexao.close()
-        print("Conexão encerrada.")
+def conectar_db():
+   try:
+      conexao = mysql.connector.connect(
+         host=os.getenv('DB_HOST'),
+         port=os.getenv('DB_PORT'),
+         user=os.getenv('DB_USER'),
+         password=os.getenv('DB_PASSWORD'),
+         database=os.getenv('DB_NAME')
+      )
+      return conexao
+   except mysql.connector.error as err:
+      print(f"Erro ao conectar ao banco de dados: {err}")
+      return None
---
+++ app/routes/note_routes.py
@@ -0,0 +1,61 @@
+from flask import Blueprint, request, jsonify
+from flask_cors import conectar_db
+
+notes_bp = Blueprint("notes_bp", __name__)
+
+
+@notes_bp.route('/notes', methods=['POST'])
+def new_note():
+    conexao = None
+    cursor = None
+
+    try:
+        data = request.get_json()
+
+        title = data.get("title")
+        text = data.get("text")
+        color = data.get("color", "")
+        user_id = data.get("user_id")
+
+        if not text or not user_id:
+            return jsonify({
+                "message": "Text and ID are mandatory"
+            }), 400
+
+        conexao = conectar_db()
+
+        if not conexao:
+            return jsonify({
+                "message": "Error connecting to database"
+            }), 500
+
+        cursor = conexao.cursor()
+
+        sql = """
+            INSERT INTO notas (titulo, conteudo, cor, usuario_id)
+            VALUES (%s, %s, %s, %s)
+        """
+
+        valores = (title, text, color, user_id)
+
+        cursor.execute(sql, valores)
+        conexao.commit()
+
+        return jsonify({
+            "message": "New note added successfully!",
+            "id_nota": cursor.lastrowid
+        }), 201
+
+    except Exception as e:
+        return jsonify({
+            "message": "Erro interno",
+            "detail": str(e)
+        }), 500
+
+    finally:
+        if cursor:
+            cursor.close()
+
+        if conexao and conexao.is_connected():
+            conexao.close()
+
